e48accd82f869c4d78ea5520904edecc63253b24
zhangkele <zhangkele1221@163.com>
2023-05-14 17:53:58 +0000

    test

--- example/echo_c++/object_pool_self.h
+++ example/echo_c++/object_pool_self.h
@@ -1,6 +1,5 @@
 
-
-#include "butil/object_pool_inl.h"
+// 关键看懂 这个链接中的一张图 就大概掌握了  整个代码 https://blog.csdn.net/KIDGIN7439/article/details/120219052
 
 #include <algorithm> // std::max, std::min
 #include <iostream>  // std::ostream
@@ -53,11 +52,11 @@ template <typename T> ObjectPoolInfo describe_objects() {
 
 
 template <typename T, size_t NITEM> struct ObjectPoolFreeChunk {
-  size_t nfree;
-  T *ptrs[NITEM];
+  size_t nfree;//表示可用对象的数量。
+  T *ptrs[NITEM];//一个大小为 NITEM 的数组，存储指向类型 T 的对象的指针。
 };
 // for gcc 3.4.5
-template <typename T> struct ObjectPoolFreeChunk<T, 0> {
+template <typename T> struct ObjectPoolFreeChunk<T, 0> {//在这个特化版本中，NITEM 为 0，因此 ptrs 数组的大小为 0。这个特化版本是为了解决某些编译器在处理大小为 0 的数组时可能出现的问题。
   size_t nfree;
   T *ptrs[0];
 };
@@ -115,6 +114,22 @@ public:
     Block() : nitem(0) {}
   };
 
+  // An Object addresses at most OP_MAX_BLOCK_NGROUP BlockGroups,
+  // each BlockGroup addresses at most OP_GROUP_NBLOCK blocks. So an
+  // object addresses at most OP_MAX_BLOCK_NGROUP * OP_GROUP_NBLOCK Blocks.
+  struct BlockGroup {
+      std::atomic<size_t> nblock;
+      std::atomic<Block*> blocks[OP_GROUP_NBLOCK];
+
+      BlockGroup() : nblock(0) {
+          // We fetch_add nblock in add_block() before setting the entry,
+          // thus address_resource() may sees the unset entry. Initialize
+          // all entries to NULL makes such address_resource() return NULL.
+          memset(static_cast<void*>(blocks), 0, sizeof(std::atomic<Block*>) * OP_GROUP_NBLOCK);
+      }
+  };
+
+
   class BAIDU_CACHELINE_ALIGNMENT LocalPool {
   public:
     explicit LocalPool(ObjectPool *pool)
@@ -140,14 +155,12 @@ public:
 #define BAIDU_OBJECT_POOL_GET(CTOR_ARGS)                                       \
   /* Fetch local free ptr */                                                   \
   if (_cur_free.nfree) {                                                       \
-    BAIDU_OBJECT_POOL_FREE_ITEM_NUM_SUB1;                                      \
     return _cur_free.ptrs[--_cur_free.nfree];                                  \
   }                                                                            \
   /* Fetch a FreeChunk from global.                                            \
      TODO: Popping from _free needs to copy a FreeChunk which is               \
      costly, but hardly impacts amortized performance. */                      \
   if (_pool->pop_free_chunk(_cur_free)) {                                      \
-    BAIDU_OBJECT_POOL_FREE_ITEM_NUM_SUB1;                                      \
     return _cur_free.ptrs[--_cur_free.nfree];                                  \
   }                                                                            \
   /* Fetch memory from local block */                                          \
@@ -209,7 +222,62 @@ public:
     Block *_cur_block;
     size_t _cur_block_index;
     FreeChunk _cur_free;
+    pthread_key_t key;
   };
+
+
+
+    // Create a Block and append it to right-most BlockGroup.
+    static Block* add_block(size_t* index) {
+        Block* const new_block = new(std::nothrow) Block;//首先，尝试创建一个新的 Block 对象，将其地址赋值给 new_block。如果创建失败（内存分配失败），则返回 NULL。
+        if (NULL == new_block) {
+            return NULL;
+        }
+        size_t ngroup;
+        do {
+            ngroup = _ngroup.load(std::memory_order_acquire);//a. 从 _ngroup 原子变量中加载当前的 block 组数量 ngroup，使用 std::memory_order_acquire 内存顺序模型。
+            if (ngroup >= 1) {//如果 ngroup 大于等于 1，获取最后一个 block 组 g。
+                BlockGroup* const g =
+                    _block_groups[ngroup - 1].load(std::memory_order_consume);
+                const size_t block_index =
+                    g->nblock.fetch_add(1, std::memory_order_relaxed);
+                if (block_index < OP_GROUP_NBLOCK) {
+                    g->blocks[block_index].store(
+                        new_block, std::memory_order_release);
+                    *index = (ngroup - 1) * OP_GROUP_NBLOCK + block_index;
+                    return new_block;
+                }
+                g->nblock.fetch_sub(1, std::memory_order_relaxed);
+            }
+        } while (add_block_group(ngroup));
+
+        // Fail to add_block_group.
+        delete new_block;
+        return NULL;
+    }
+
+    // Create a BlockGroup and append it to _block_groups.
+    // Shall be called infrequently because a BlockGroup is pretty big.
+    static bool add_block_group(size_t old_ngroup) {
+        BlockGroup* bg = NULL;
+        //BAIDU_SCOPED_LOCK(_block_group_mutex);
+        std::lock_guard<std::mutex> scoped_locker_dummy_at_line_43(_block_group_mutex);
+        const size_t ngroup = _ngroup.load(butil::memory_order_acquire);
+        if (ngroup != old_ngroup) {
+            // Other thread got lock and added group before this thread.
+            return true;
+        }
+        if (ngroup < OP_MAX_BLOCK_NGROUP) {
+            bg = new(std::nothrow) BlockGroup;
+            if (NULL != bg) {
+                // Release fence is paired with consume fence in add_block()
+                // to avoid un-constructed bg to be seen by other threads.
+                _block_groups[ngroup].store(bg, butil::memory_order_release);
+                _ngroup.store(ngroup + 1, butil::memory_order_release);
+            }
+        }
+        return bg != NULL;
+    }
 
   inline LocalPool *get_or_new_local_pool() {//通过这个函数，可以确保每个线程都有一个与之关联的 LocalPool 实例，并且在线程退出时正确地删除这些实例。
     LocalPool *lp = _local_pool;
@@ -225,7 +293,11 @@ public:
     std::lock_guard<std::mutex> scoped_locker_dummy_at_line_42(
         _change_thread_mutex);
     _local_pool = lp;
-    butil::thread_atexit(LocalPool::delete_local_pool, lp);//线程退出时候 处理的一些任务 pthrea的库中有类似的函数  以便在当前线程退出时自动删除关联的 LocalPool 实例
+    //butil::thread_atexit(LocalPool::delete_local_pool, lp);//线程退出时候 处理的一些任务 pthrea的库中有类似的函数  以便在当前线程退出时自动删除关联的 LocalPool 实例
+    // 这两个pthread 代替上面 thread_atexit
+    pthread_setspecific(key, lp);
+    // 在程序的某个地方初始化键，并提供清理函数。
+    pthread_key_create(&key, LocalPool::delete_local_pool);
    //_nlocal.fetch_add(1, butil::memory_order_relaxed);//使用 fetch_add 函数原子地递增 _nlocal 成员变量的值。butil::memory_order_relaxed 表示使用松散的内存顺序，这意味着在递增 _nlocal 时不需要保证其他内存操作的顺序。
     _nlocal.fetch_add(1, std::memory_order_relaxed);
     return lp;
@@ -257,7 +329,7 @@ public:
 
 private:
     ObjectPool() {
-        _free_chunks.reserve(OP_INITIAL_FREE_LIST_SIZE);
+        _free_chunks.reserve(OP_INITIAL_FREE_LIST_SIZE);// 1024
         pthread_mutex_init(&_free_chunks_mutex, NULL);
     }
 
@@ -266,18 +338,42 @@ private:
     }
 
 
+
+    bool pop_free_chunk(FreeChunk& c) {// free_chunks 初始化时是空，只reserve了一个空间 所以 pop_free_chunk 也会失败，当不为空时候，会拿到一个free_chunk，然后拷贝到LocalPool的cur_free中
+        // Critical for the case that most return_object are called in
+        // different threads of get_object.
+        if (_free_chunks.empty()) {
+            return false;
+        }
+        pthread_mutex_lock(&_free_chunks_mutex);
+        if (_free_chunks.empty()) {
+            pthread_mutex_unlock(&_free_chunks_mutex);
+            return false;
+        }
+        DynamicFreeChunk* p = _free_chunks.back();
+        _free_chunks.pop_back();
+        pthread_mutex_unlock(&_free_chunks_mutex);
+        c.nfree = p->nfree;
+        memcpy(c.ptrs, p->ptrs, sizeof(*p->ptrs) * p->nfree);
+        free(p);
+        return true;
+    }
+
   static std::atomic<ObjectPool*> _singleton;
   static pthread_mutex_t _singleton_mutex;
 
   static std::atomic<long> _nlocal;
   static __thread LocalPool* _local_pool;
+  static std::atomic<size_t> _ngroup;
 
   static pthread_mutex_t _change_thread_mutex;
+  static pthread_mutex_t _block_group_mutex;
 
+  static std::atomic<BlockGroup*> _block_groups[OP_MAX_BLOCK_NGROUP];
 
-  std::vector<DynamicFreeChunk*> _free_chunks;
-  pthread_mutex_t _free_chunks_mutex;
-  
+  std::vector<DynamicFreeChunk*> _free_chunks;//一个存储空闲对象块的向量  其元素类型为指向 DynamicFreeChunk 类型的指针。这个向量用于存储空闲的动态内存块，即不再使用的 DynamicFreeChunk 对象。这样在需要新的空闲块时，可以从这个向量中获取，而不是分配新的内存。同样，在释放内存块时，可以将它们添加回这个向量，以便将来重用。这有助于减少内存分配和释放的开销。
+  pthread_mutex_t _free_chunks_mutex;//互斥锁，用于保护 _free_chunks 的访问
+
 };
 
 
@@ -293,7 +389,16 @@ template <typename T>
 std::atomic<long> ObjectPool<T>::_nlocal = 0;
 
 template <typename T>
+std::atomic<size_t> ObjectPool<T>::_ngroup = 0;
+
+template <typename T>
 pthread_mutex_t ObjectPool<T>::_singleton_mutex = PTHREAD_MUTEX_INITIALIZER;
+
+template <typename T>
+pthread_mutex_t ObjectPool<T>::_block_group_mutex = PTHREAD_MUTEX_INITIALIZER;
+
+template <typename T>
+pthread_mutex_t ObjectPool<T>::_change_thread_mutex = PTHREAD_MUTEX_INITIALIZER;
 
 
 } // namespace butil
