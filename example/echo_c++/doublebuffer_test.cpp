3fe90095c536566acc54d269501c09ce92478a8a
zhangkele <zhangkele1221@163.com>
2023-06-19 04:52:19 +0000

    1

--- example/echo_c++/doublebuffer_test.cpp
+++ example/echo_c++/doublebuffer_test.cpp
@@ -1,8 +1,20 @@
 #include<iostream>
 #include<limits.h>
 
+#include "../../src/butil/DoublyBufferedData.h"
+
+
+struct Foo {
+    Foo() : x(0) {}
+    int x;
+};
+
+
 int main(){
     std::cout << "current PTHREAD_KEYS_MAX: " << PTHREAD_KEYS_MAX << std::endl;
+
+    butil::DoublyBufferedData<Foo> data[PTHREAD_KEYS_MAX + 1];
+    butil::DoublyBufferedData<Foo>::ScopedPtr ptr;
 
     return 1;
 }
