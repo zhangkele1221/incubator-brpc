4e2cb8d1348883ecf46329cf5141b83e807b0487
zhangkele <zhangkele1221@163.com>
2023-06-20 12:12:52 +0000

    1

--- example/echo_c++/doublebuffer_test.cpp
+++ example/echo_c++/doublebuffer_test.cpp
@@ -1,8 +1,11 @@
 #include<iostream>
 #include<limits.h>
 
-#include "../../src/butil/DoublyBufferedData.h"
-
+//  动态库编译方法
+//g++ doublebuffer_test.cpp  -I/workspace/incubator-brpc/output/include   -L/workspace/incubator-brpc/output/lib -lbrpc  -o your_program 
+// export LD_LIBRARY_PATH=/path/to/brpc/output/lib:$LD_LIBRARY_PATH  环境变量制定动态库的路径
+//#include "../../src/butil/DoublyBufferedData.h"
+#include <butil/containers/doubly_buffered_data.h>
 
 struct Foo {
     Foo() : x(0) {}
